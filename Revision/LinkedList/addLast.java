1fc4fb1a9e823a560a76cd06e72bdb21119cd5f4
Amit Anand <dropmail.amit@gmail.com>
2022-01-03 21:03:05 +0530

    still not done.

---
+++ Revision/LinkedList/addLast.java
@@ -0,0 +1,62 @@
+package Revision.LinkedList;
+
+import java.util.Scanner;
+
+public class addLast {
+    public static class Node {
+        int data;
+        Node next;
+    }
+    public static class LinkedList{
+        int size;
+        Node head;
+        Node tail;
+        void addLast(int val){
+            Node node = new Node();
+            node.data = val;
+            node.next = null;
+            
+            if(size == 0){
+                head = tail = node;
+            }else{
+                tail.next = node;
+                tail = node;
+            }
+            size++;
+        }
+        public int size(){
+            return size;
+        }
+        public void display(){
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void testList(LinkedList list){
+        for(Node temp = list.head; temp != null; temp = temp.next){
+            System.out.println(temp.data);
+        }
+        System.out.println(list.size);
+        if(list.size > 0){
+            System.out.println(list.tail.data);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        String str = scn.nextLine();
+        while(str.equals("quit") == false){
+            if(str.startsWith("addLast")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            }
+            str = scn.nextLine();
+        }
+        testList(list);
+        scn.close();
+    }
+}
