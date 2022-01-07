9a2f812dcd60dacfdae92d60c3b652632bf694b4
Amit Anand <dropmail.amit@gmail.com>
2022-01-07 19:08:27 +0530

    little changes.

--- graphs/myGraph.java
+++ graphs/myGraph.java
@@ -1,8 +1,12 @@
 import java.util.*;
 
 public class myGraph{
-    static class Edge{
-        int src; int nbr; int wt;
+
+    public static class Edge{
+        int src;
+        int nbr; 
+        int wt;
+
         Edge(int src, int nbr, int wt){
             this.src = src;
             this.nbr = nbr;
@@ -10,5 +14,12 @@ public class myGraph{
         }
     }
 
-    
+    public static ArrayList<Edge> graph;
+
+    public static void fun(){
+        
+    }
+    public static void main(String[] args){
+        fun();
+    }
 }
