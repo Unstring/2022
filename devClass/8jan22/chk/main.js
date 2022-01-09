5deaf71739c1c10e6cf5155da073214a356c5e05
Amit Anand <dropmail.amit@gmail.com>
2022-01-09 16:38:37 +0530

    changes.

--- devClass/8jan22/chk/main.js
+++ devClass/8jan22/chk/main.js
@@ -4,17 +4,30 @@
     let divbreadcrumb = document.querySelector("#breadcrumb");
     let divContainer = document.querySelector("#container");
     let templates = document.querySelector("#templates");
+    let resources = [];
+    let 
 
     btnAddFolder.addEventListener("click", addFolder);
     btnAddTextFile.addEventListener("click", addTextFile);
 
     function addFolder(){
+        
+
+
+
         let fname = prompt("Enter folder's name");
         
         let divFolderTemplate = templates.content.querySelector(".folder");
         let divFolder = document.importNode(divFolderTemplate, true);
 
+        let spanRename = divFolder.querySelector("[action=rename]");
+        let spanDelete = divFolder.querySelector("[action=delete]");
+        let spanView = divFolder.querySelector("[action=view]");
         let divName = divFolder.querySelector("[purpose=name]");
+
+        spanRename.addEventListener("click", renameFolder);
+        spanDelete.addEventListener("click", deleteFolder);
+        spanView.addEventListener("click", viewFolder)
         divName.innerHTML = fname;
         
         divContainer.appendChild(divFolder);
@@ -26,7 +39,7 @@
     }
 
     function deleteFolder(){
-
+        console.log("in delete");
     }
 
     function deleteTextFile(){
@@ -34,7 +47,8 @@
     }
 
     function renameFolder(){
-
+        
+        console.log("in rename");
     }
 
     function renameTextFile(){
@@ -42,7 +56,7 @@
     }
 
     function viewFolder(){
-
+        console.log("in view");
     }
 
     function viewTextFile(){
@@ -50,7 +64,8 @@
     }
 
     function saveToStorage(){
-
+        let rjson = JSON.stringify(resources);
+        localStorage.setItem("data", rjson)
     }
 
     function loadFromStorage(){
